ab72b22019fa9ecfc93546e58e67b2198bb134ef
sameerakmal <sameer2akhmal@gmail.com>
2024-08-04 23:27:39 +0530

    Pattern printing

---
+++ IOI/Patterns/IsoscelesTriangle.java
@@ -0,0 +1,14 @@
+
+public class IsoscelesTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int k = 1; k <= 5-i+1; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
